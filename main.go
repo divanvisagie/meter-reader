d901338b7978399c1d96fae10acc6125146c37a1
Divan Visagie <visagiedivan@gmail.com>
2018-05-31 16:04:46 +0200

    break out function

--- main.go
+++ main.go
@@ -4,25 +4,27 @@ import (
 	"gocv.io/x/gocv"
 )
 
-func rotate() {
-
-}
-
-func main() {
-
+func process(filePath string) gocv.Mat {
 	var CANNY1 float32 = 100.0
 	var CANNY2 float32 = 200.0
 
-	img := gocv.IMRead("data/meter1.jpg", gocv.IMReadGrayScale)
+	img := gocv.IMRead("data/meter1.jpg", gocv.IMReadReducedGrayscale4)
 
 	edges := gocv.NewMat()
 
 	gocv.Canny(img, &edges, CANNY1, CANNY2)
 
+	return edges
+}
+
+func main() {
+
 	window := gocv.NewWindow("Image")
 	defer window.Close()
 
-	window.IMShow(edges)
+	image := process("data/meter1.jpg")
+
+	window.IMShow(image)
 
 	for {
 
